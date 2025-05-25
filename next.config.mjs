4f8a5ff3c7c0f5b19861fdb81888bbf596d05663
hxrry27 <131454517+hxrry27@users.noreply.github.com>
2025-05-25 13:57:02 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 const nextConfig = {
   env: {
-    COOKIE_SECRET: process.env.COOKIE_SECRET,
+    COOKIE_SECRET: process.env.COOKIE_SECRET, //not used
     RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
   },
   experimental: {
